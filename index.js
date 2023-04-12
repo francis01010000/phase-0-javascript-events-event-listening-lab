092195d45bf482d9a903b5cb11779690e9dd5f40
Francisco Perez <Franciscop693@gmail.com>
2023-04-11 23:00:02 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    const clickAlert = () => alert('I was clicked!');
+
+    input.addEventListener('click', clickAlert);
 }
